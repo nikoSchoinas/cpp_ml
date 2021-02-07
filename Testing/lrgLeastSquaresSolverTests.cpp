f4c993bf093d13b7762a061b5e478058c60402cf
Nikolaos Schoinas <dai16167@uom.edu.gr>
2021-02-07 10:40:26 +0000

    Create a file and add a test for lrgLinearDataCreator class instantiation.

---
+++ Testing/lrgLeastSquaresSolverTests.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+#include "lrgCatchMain.h"
+#include "lrgLinearDataCreator.h"
+
+
+TEST_CASE( "lrgLinearDataCreator class instantiation", "[lrgLinearDataCreator]" ) {
+  lrgLinearDataCreator d;
+} 
