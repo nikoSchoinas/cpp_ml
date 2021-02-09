1b79e82e0b54ad8be211b1b3f2a01411b44f866c
Nikolaos Schoinas <dai16167@uom.edu.gr>
2021-02-09 11:25:20 +0000

    Implement GetData method of lrgLinearDataCreator class.

--- Code/Lib/lrgLinearDataCreator.cpp
+++ Code/Lib/lrgLinearDataCreator.cpp
@@ -1,6 +1,36 @@
 #include "lrgLinearDataCreator.h"
-
+#include <random>
+#include <functional>
 
 lrgLinearDataCreator::lrgLinearDataCreator(){}
-lrgLinearDataCreator::~lrgLinearDataCreator(){}
-std::vector<std::pair<double, double> > lrgLinearDataCreator::GetData(){}
+lrgLinearDataCreator::~lrgLinearDataCreator() {}
+std::vector<std::pair<double, double>> lrgLinearDataCreator::GetData() 
+{
+    // We could use any generator.
+    // std::default_random_engine generator;
+    std::mt19937_64 mt64;
+
+    // We use as a teplate the code example of "Hands-On Machine Learning" book, p.108,
+    // where the noise is a random positive number in [0,1) from a uniform distribution.
+    // That's from numpy.random.rand documentation.
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+
+    // Bind the generator and the distribution together. It's more practical.
+    auto rand_num = std::bind(distribution, mt64);
+
+    // Declare x, y, noise variables outside of the for-loop.
+    // In that way we can change their values inside for-loop without create new variables.
+    double x;
+    double y;
+    double noise;
+
+    for (unsigned int i = 0; i < m_size; i++)
+    {
+        x = rand_num();
+        noise = rand_num();
+        y = m_t1*x + m_t0 + noise;
+        (*m_vec).push_back(std::make_pair(x,y));
+    }
+
+    return (*m_vec);
+}
--- Code/Lib/lrgLinearDataCreator.h
+++ Code/Lib/lrgLinearDataCreator.h
@@ -1,16 +1,20 @@
 #ifndef lrgLinearDataCreator_h
 #define lrgLinearDataCreator_h
 #include "lrgDataCreatorI.h"
-
+#include <memory>
 class lrgLinearDataCreator : public lrgDataCreatorI
 {
 public:
-    lrgLinearDataCreator(/* args */);
+    lrgLinearDataCreator();
     ~lrgLinearDataCreator();
-    std::vector<std::pair<double, double> > GetData();
+    std::vector<std::pair<double, double>> GetData();
+    void GenerateData();
+
+private:
+    unsigned int m_size;
+    double m_t0;
+    double m_t1;
+    std::unique_ptr<std::vector<std::pair<double, double>>> m_vec;
 };
-
-
-
 
 #endif
